b403b487d6f3adae609448969374f27fad54f2f0
lberrymage <logan@lberrymage.dev>
2024-02-24 22:17:43 -0700

    Build ina native library with Android library
    Only aarch64 is supported for now. This isn't due to any technical
    limitation, but because there isn't much reason to support other
    architectures at the moment. They can be added later as needed.
    
    No symbols are exported in libina.so currently. This commit just ensures
    the build system builds the native library and includes libina.so in the
    AAR.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,17 +2,26 @@
 //
 // SPDX-License-Identifier: LicenseRef-Proprietary
 
+import org.apache.tools.ant.taskdefs.condition.Os
+import org.apache.tools.ant.taskdefs.condition.Os.FAMILY_MAC
+import org.apache.tools.ant.taskdefs.condition.Os.FAMILY_UNIX
+import org.apache.tools.ant.taskdefs.condition.Os.FAMILY_WINDOWS
+
 plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.android.library)
 }
 
+val inaMinSdk = 29
+
 android {
     namespace = "app.accrescent.ina"
     compileSdk = 34
 
+    ndkVersion = "26.2.11394342"
+
     defaultConfig {
-        minSdk = 29
+        minSdk = inaMinSdk
     }
 
     buildTypes {
@@ -28,4 +37,46 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+}
+
+tasks.register<Exec>("buildJniLibs") {
+    // Obtain the host tag for the current system so we can find the proper toolchain directory
+    //
+    // https://developer.android.com/ndk/guides/other_build_systems#overview
+    val hostTag = when {
+        Os.isFamily(FAMILY_MAC) -> "darwin-x86_64"
+        Os.isFamily(FAMILY_UNIX) -> "linux-x86_64"
+        Os.isFamily(FAMILY_WINDOWS) -> "windows-x86_64"
+        else -> throw TaskExecutionException(this, Exception("unsupported host platform"))
+    }
+    val toolchainDir = "${android.ndkDirectory}/toolchains/llvm/prebuilt/$hostTag/bin"
+    val api = inaMinSdk
+
+    val aarch64CcPath = "$toolchainDir/aarch64-linux-android$api-clang"
+    environment("AR", "$toolchainDir/llvm-ar")
+    environment("CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER", aarch64CcPath)
+    environment("CC_aarch64-linux-android", aarch64CcPath)
+
+    commandLine("cargo", "build", "-p", "ina", "--target", "aarch64-linux-android", "--release")
+
+    doLast {
+        copy {
+            from("$rootDir/target/aarch64-linux-android/release/libina.so")
+            into("$projectDir/src/main/jniLibs/arm64-v8a")
+        }
+    }
+}
+
+tasks.register<Exec>("cargoClean") {
+    commandLine("cargo", "clean")
+}
+
+tasks.preBuild {
+    dependsOn(tasks.getByName("buildJniLibs"))
+}
+
+tasks.clean {
+    dependsOn(tasks.getByName("cargoClean"))
+
+    delete(fileTree("$projectDir/src/main/jniLibs"))
 }
